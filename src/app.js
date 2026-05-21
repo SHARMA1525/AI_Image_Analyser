9d1dead75b91c4cd702c2663a0b09e714b448096
SHARMA1525 <saipraneethsharma57@gmail.com>
2026-05-21 11:54:58 +0530

    Made some required changes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const uploadRoutes = require('./routes/upload.routes');
 const { errorHandler } = require('./middlewares/error.middleware');
 const logger = require('./config/logger');
 
 const app = express();
+
+// Ensure uploads directory exists
+const uploadsDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 app.use(helmet());
 app.use(cors());
